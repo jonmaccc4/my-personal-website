938e13432f6227ecd77115682acf951bb3bf5bc6
jondev <jonmac2454@gmail.com>
2025-03-03 11:31:59 +0300

    my personal project

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+const  navbar = document.getElementById("navbar")
+ 
+navbar.style.textAlign = "center"
+
+const about = document.getElementById("about")
+
+about.style.backgroundColour ="#6dbb0e"
